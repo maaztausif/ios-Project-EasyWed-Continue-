9cb2470d48e3fc03a6a4931a752244542e4da4ed
maaztausif <maaztausif48@gmail.com>
2020-08-04 12:45:18 -0700

    images change in cell complete

--- Easy_Wedding/Controller/ManagerGetHallViewController.swift
+++ Easy_Wedding/Controller/ManagerGetHallViewController.swift
@@ -20,14 +20,20 @@ class ManagerGetHallViewController: UIViewController,UITableViewDataSource,UITab
     var databaseRef: DatabaseReference!
     var image:UIImage! = nil
     var photoArray = [UIImage]()
+    var documentArray = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        retrieveImages()
+
         hallTableView.delegate = self
         hallTableView.dataSource = self
 //        hallTableView.separatorStyle = .none
-        retrieveImages()
+        retrieveDocuments()
         retrieveData()
+        
+        
         
         hallTableView.register(UINib(nibName: "HallInfoTableViewCell", bundle: nil), forCellReuseIdentifier: "hallCell")
         hallTableView.reloadData()
@@ -68,50 +74,127 @@ class ManagerGetHallViewController: UIViewController,UITableViewDataSource,UITab
         let cell = tableView.cellForRow(at: indexPath) as! HallInfoTableViewCell
 
         print("\(cell.lbl_HallName.text)========select row at")
+        print(" hall 1 = \(hallArray[0].hall_Name)")
+        print(" hall 2 = \(hallArray[1].hall_Name)")
+        print(" \(hallArray[indexPath.row].hall_Name)")
+        print("hallArray =count = \(hallArray.count)")
+        print(" hall 1 = \(documentArray[0])")
+        print(" hall 2 = \(documentArray[1])")
+        print(" hall 3 = \(documentArray[2])")
+        self.hallTableView.reloadData()
+
     }
     
     func retrieveData(){
         print("retrieve data =============")
         let hallInfo = HallInfo()
-        let docRef = db.collection("user:\(auth)").document("grace")
+        var i = 0
+        
+        
+        db.collection("user:\(auth)").getDocuments { (ducomentSnapshot, error) in
+            if error != nil{
+                
+            }else{
 
-        DispatchQueue.global(qos:.userInteractive).sync {
-                    docRef.getDocument { (document, error) in
-                if let document = document, document.exists {
-                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                    hallInfo.hall_Name = document["Hall Name"] as! String
-                    hallInfo.no_Of_People = document["No Of People"] as! String
-                    hallInfo.rupees = document["Phone No"] as! String
-                    print("Document data: \(dataDescription)")
-                    print("\(hallInfo.hall_Name)===================")
-                    self.hallArray.append(hallInfo)
-                    print("hall name of hall array = \(self.hallArray[0].hall_Name)=============")
-                    self.hallTableView.reloadData()
-
-                } else {
-                    print("Document does not exist")
-                }
+//                for document in ducomentSnapshot!.documents {
+//                      print("\(document.documentID) => \(document.data())")
+//                    let docRef = self.db.collection("user:\(self.auth)").document("\(document.documentID)")
+//                                docRef.getDocument { (document, error) in
+//                            if let document = document, document.exists {
+//                                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                                hallInfo.hall_Name = document["Hall Name"] as! String
+//                                hallInfo.no_Of_People = document["No Of People"] as! String
+//                                hallInfo.rupees = document["Phone No"] as! String
+//                                print("Document data: \(dataDescription)")
+//                                print("\(hallInfo.hall_Name)===================")
+//                                self.hallArray.append(hallInfo)
+//                                print("hall name of hall array = \(self.hallArray[0].hall_Name)=============")
+//                                self.hallTableView.reloadData()
+//
+//                            } else {
+//                                print("Document does not exist")
+//                            }
+//                        }
+//                    print("===================================================")
+//                    self.hallTableView.reloadData()
+//
+//                    //print("hallArray = \(self.hallArray[i])")
+//                    i += 1
+//                  }
+                
+                //check
+//                for i in self.documentArray{
+//
+//                                        let docRef = self.db.collection("user:\(self.auth)").document("\(i)")
+//                                                    docRef.getDocument { (document, error) in
+//                                                if let document = document, document.exists {
+//                                                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                                                    hallInfo.hall_Name = document["Hall Name"] as! String
+//                                                    hallInfo.no_Of_People = document["No Of People"] as! String
+//                                                    hallInfo.rupees = document["Phone No"] as! String
+//                                                    print("Document data: \(dataDescription)")
+//                                                    print("\(hallInfo.hall_Name)===================")
+//                                                    self.hallArray.append(hallInfo)
+//                                                    print("hall name of hall array = \(self.hallArray[0].hall_Name)=============")
+//                                                    self.hallTableView.reloadData()
+//
+//                                                } else {
+//                                                    print("Document does not exist")
+//                                                }
+//                                            }
+//                }
+                
             }
         }
         
-
 //        self.hallArray.append(hallInfo)
 //        print("hall name of hall array = \(hallArray[0].hall_Name)=============")
 //        self.hallTableView.reloadData()
     }
     
+    func retrieveDocuments(){
+        
+        print("retrieve Document data =============")
+        let hallInfo = HallInfo()
+        
+        db.collection("user:\(auth)").getDocuments { (ducomentSnapshot, error) in
+                   if error != nil{
+                       
+                   }else{
+                    
+                    for document in ducomentSnapshot!.documents {
+                          print("\(document.documentID) => \(document.data())")
+                        self.documentArray.append(document.documentID)
+//                        print("hall name=\(document["Hall Name"])!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        hallInfo.hall_Name = document["Hall Name"] as! String
+                        hallInfo.no_Of_People = document["No Of People"] as! String
+                        hallInfo.rupees = document["Phone No"] as! String
+//                        print("Document data: \(dataDescription)")
+                        print("\(hallInfo.hall_Name)===================")
+                        self.hallArray.append(hallInfo)
+                        print("hall name of hall array = \(self.hallArray[0].hall_Name)=============")
+
+                    }
+//                    self.hallTableView.reloadData()
+
+            }
+        
+        }
+        self.hallTableView.reloadData()
+
+    }
     
     func retrieveImages(){
         print("retrieve images")
         
         
-        for i in 0...15{
+        for var i in 0...15{
             let storageRef = Storage.storage().reference().child("user:\(auth)/myImage\(i)")
                     storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
                       if  error != nil {
                         // Uh-oh, an error occurred!
                         print("/ Uh-oh, an error occurred!")
-                        
+                        i = 16
                       } else {
                         // Data for "images/island.jpg" is returned
                         self.image = UIImage(data: data!)
